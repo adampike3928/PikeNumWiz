a6bbebf3fc437a9891783d92d43d0757c40024bd
adampike334 <adampikespam@gmail.com>
2020-02-04 15:45:46 -0500

    added 4 comments to console

---
+++ Assets/NumberWizard.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {//prints the text inputed
+        Debug.Log("Sup, this is num wiz bro.");
+        Debug.Log("Pick a number my dude or dudet");
+        Debug.Log("Whats the highest number?:");
+        Debug.Log("Whats the smallest number?:");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
