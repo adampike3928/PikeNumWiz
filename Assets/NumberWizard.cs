78ba3ef5d79a6bcc84ca12703e1e8e92feac9278
adampike334 <adampikespam@gmail.com>
2020-02-06 15:50:27 -0500

    Added Void statments, cleaned up on code, added comments, able to loop back to beginning

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -4,49 +4,65 @@ using UnityEngine;
 
 public class NumberWizard : MonoBehaviour
 {//varables that are globle to the class
-    int intmax = 1000;
-    int intmin = 1;
-    int intguess = 500;
+    int intmax;
+    int intmin;
+    int intguess;
     // Start is called before the first frame update
+  
     void Start()
+    {//calls on VoidGameStart commands
+        VoidStartGame();
+    }
+    void VoidStartGame()
     {
-        
+        intmax = 1000;
+        intmin = 1;
+        intguess = 500;
+
         //Greetings comments
         Debug.Log("Sup, this is num wiz bro. ");
         Debug.Log("Pick a number my dude or dudet ");
         Debug.Log("The highest number you may input is " + intmax);
         Debug.Log("The lowest number you can put in is " + intmin);
-       //Instructions for input
+        //Instructions for input
         Debug.Log("is your number higher or lower than 500?");
         Debug.Log("Up = higher, down = lower, enter = correct");
-       //Bugfix for not going to 1000
+        //Bugfix for not going to 1000
         intmax = intmax + 1;
     }
-
     // Update is called once per frame
     void Update()
     {//In put keys, update constantly checked
         //Cheacks for up arrow input
         if (Input.GetKeyDown(KeyCode.UpArrow)) 
         {
-            Debug.Log("Up input has been read");
+            
             intmin = intguess;
-            intguess = (intmax + intmin) / 2;
-            Debug.Log(intguess);
+            VoidNextGuess();
         }
         //Cheacks for down arrow input
         else if (Input.GetKeyDown(KeyCode.DownArrow)) 
         {
-            Debug.Log("Down input has been read");
+            
             intmax = intguess;
-            intguess = (intmax + intmin) / 2;
-            Debug.Log(intguess);
+            VoidNextGuess();
             
         }
         //Cheacks for enter key input
         else if (Input.GetKeyDown(KeyCode.Return))
         {
-            Debug.Log("Enter input has been read");
+            intmax = intguess;
+            VoidNextGuess();
+            //Starts game again
+            VoidStartGame();
         }
     }
+    //Void to simplfy input commands
+    void VoidNextGuess()
+    {
+        intguess = (intmax = intmin) / 2;
+        Debug.Log("Is it higher or lower than" + intguess);
+    }
+
 }
+
