fdf2f93413ae20d99ca5aafad21f092e90c9a826
adampike334 <adampikespam@gmail.com>
2020-02-06 15:06:58 -0500

    Added Varables as well as inputs

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -7,15 +7,32 @@ public class NumberWizard : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {//prints the text inputed
-        Debug.Log("Sup, this is num wiz bro.");
-        Debug.Log("Pick a number my dude or dudet");
-        Debug.Log("Whats the highest number?:");
-        Debug.Log("Whats the smallest number?:");
+        int intmax = 1000;
+        int intmin = 1;
+        Debug.Log("Sup, this is num wiz bro. ");
+        Debug.Log("Pick a number my dude or dudet ");
+        Debug.Log("The highest number you may input is " + intmax);
+        Debug.Log("The lowest number you can put in is " + intmin);
+       //Instructions for input
+        Debug.Log("is your number higher or lower than 500?");
+        Debug.Log("Up = higher, down = lower, enter = correct");
+
     }
 
     // Update is called once per frame
     void Update()
-    {
-        
+    {//In put keys, update constantly checked
+        if (Input.GetKeyDown(KeyCode.UpArrow)) 
+        {
+            Debug.Log("Up Arrow key was pressed"); 
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow)) 
+        {
+            Debug.Log("Down Arrow key pressed");
+        }
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("Enter key pressed");
+        }
     }
 }
